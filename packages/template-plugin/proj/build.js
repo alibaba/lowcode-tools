d963935e171d99cd36a4c2234d8a15bccdfa172f
LeoYuan 袁力皓 <yuanlihao1988@gmail.com>
2022-11-04 15:18:26 +0800

    fix: build.js should export config

--- packages/template-plugin/proj/build.js
+++ packages/template-plugin/proj/build.js
@@ -1,5 +1,6 @@
-{
+module.exports = {
   plugins: [
+    'build-plugin-fusion',
     [
       '@alilc/build-plugin-alt',
       {
@@ -9,12 +10,6 @@
         // 配置要打开的页面，在注入调试模式下，不配置此项的话不会打开浏览器
         // 支持直接使用官方 demo 项目：https://lowcode-engine.cn/demo/index.html
         // openUrl: 'https://lowcode-engine.cn/demo/index.html?debug',
-      }
-    ],
-    [
-      'build-plugin-fusion',
-      {
-        themePackage: '@alifd/theme-lowcode-light'
       }
     ]
   ]
